831d5b16c545199e3b686605276a7755d91cb23e
otTATto <TATo123SU@gmail.com>
2024-10-21 18:23:36 +0900

    画面遷移の実装
    基本的な画面遷移の解答例.txt の手順をすべて踏んでみた

---
+++ Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
@@ -0,0 +1,7 @@
+// シーンの名前を定義したクラス
+public class SceneNames
+{
+    public const string GameScene = "_Complete-Game";
+    public const string TitleScene = "TitleScene";
+    public const string HomeScene = "HomeScene";
+}
