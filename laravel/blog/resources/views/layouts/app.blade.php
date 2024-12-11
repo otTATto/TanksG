d8b76994194fd7a41ab7b4ccc2f36a815200a9ff
otTATto <TATo123SU@gmail.com>
2024-12-11 23:56:21 +0900

    add: Bootstrap の導入
    CSS フレームワークによる管理画面の見やすさの向上

--- laravel/blog/resources/views/layouts/app.blade.php
+++ laravel/blog/resources/views/layouts/app.blade.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/main.css">
 </head>
 <body>
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <header>
         <a href="/" class="site-title">Laravel</a>
         <nav class="tab">
